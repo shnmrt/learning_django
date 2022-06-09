065de71d8fb9021f28de3c90113fe39e7f5d30ef
Murat Şahin <sahinmurat3@gmail.com>
2022-06-09 13:01:18 +0300

    function based view

---
+++ my_site/first_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+# domain.com/first_app/simple_view
+urlpatterns = [
+    path('', views.simple_view)
+]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+# Create your views in here 
+
+def simple_view(request):
+    return HttpResponse('SIMPLE VIEW') # TEMPLATE HTML (JINJA)
+
