3e6fb8770fb22d2c5ca155a0730ac9198192e641
Murat Şahin <sahinmurat3@gmail.com>
2022-06-09 14:18:32 +0300

    update dynamic view

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 # domain.com/first_app/simple_view
 urlpatterns = [
-    path('', views.simple_view)
+    #path('sports/', views.sports_view),
+    #path('finance/', views.finance_view),
+    path('<str:topic>/', views.news_view), # dynamic view
+    path('<int:num1>/<int:num2>', views.add_view)
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -3,6 +3,25 @@ from django.http.response import HttpResponse
 
 # Create your views in here 
 
-def simple_view(request):
-    return HttpResponse('SIMPLE VIEW') # TEMPLATE HTML (JINJA)
+articles = {
+    'sports':'Sports Page',
+    'finance':'Finance Page',
+    'politics': 'Politics Page'
+}
 
+"""
+# function based view
+def sports_view(request):
+    return HttpResponse(articles['sports']) # TEMPLATE HTML (JINJA)
+
+def finance_view(request):
+    return HttpResponse(articles['finance'])
+"""
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
+
+def add_view(request, num1, num2):
+    # domain.com/first_app/num1/num2 --> num1+num2
+    add_result = num1 + num2
+    result = f"{num1}+{num2} = {add_result}"
+    return HttpResponse(str(result))
