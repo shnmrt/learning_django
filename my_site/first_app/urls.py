a11278b062022687cfa897ac81665b669c8efc4f
Murat Şahin <sahinmurat3@gmail.com>
2022-06-09 16:51:37 +0300

    connectng a view template. not best practice

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 # domain.com/first_app/simple_view
 urlpatterns = [
-    #path('sports/', views.sports_view),
-    #path('finance/', views.finance_view),
-    #path('<int:num1>/<int:num2>', views.add_view)
-    path('<int:num_page>', views.num_page_view), # url redirect
-    path('<str:topic>/', views.news_view, name='topic-page'), # dynamic view # name is for reverse look from viem to urls 
+    path('', views.simple_view) # domain.com/first_app
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -4,40 +4,5 @@ from django.http.response import HttpResponse
 from django.urls import reverse
 # Create your views in here 
 
-articles = {
-    'sports':'Sports Page',
-    'finance':'Finance Page',
-    'politics': 'Politics Page'
-}
-
-"""
-# function based view
-def sports_view(request):
-    return HttpResponse(articles['sports']) # TEMPLATE HTML (JINJA)
-
-def finance_view(request):
-    return HttpResponse(articles['finance'])
-"""
-def news_view(request, topic):
-    try:
-        #result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        raise Http404('404 GENERIC ERROR') # 404.html 
-
-# domain.com/first_app/0 ----> domain.com/first_app/sports
-
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys()) # ['sports','finance','politics']
-    topic = topics_list[num_page]
-    
-    
-    return HttpResponseRedirect(reverse('topic-page',args=[topic]))
-
-"""
-def add_view(request, num1, num2):
-    # domain.com/first_app/num1/num2 --> num1+num2
-    add_result = num1 + num2
-    result = f"{num1}+{num2} = {add_result}"
-    return HttpResponse(str(result))
-"""
+def simple_view(request):
+    return render(request, 'first_app/example.html') # .html
