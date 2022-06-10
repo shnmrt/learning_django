5ec1c2eb38c68c31278291d42f92781b03a37783
Murat Şahin <sahinmurat3@gmail.com>
2022-06-10 12:59:37 +0300

    static files

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c5s(awlz16u!#el*x0it+$t3otst%7lu4-jsuy65ynh#)feyfg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False#True
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []#['127.0.0.1']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+#first_app/static/first_app/static/django.jpg
 STATIC_URL = 'static/'
 
 # Default primary key field type
